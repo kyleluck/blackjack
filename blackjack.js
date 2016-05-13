f951dc4fbb14397dbd8f7d74d24bb305d17c7344
Kyle Luck <luck.kyle@gmail.com>
2016-05-13 16:36:40 -0400

    design touches

---
+++ blackjack.js
@@ -0,0 +1,10 @@
+var card = {suit: 'Diamond', point: 10};
+//deck & hand are arrays of objects ^
+//function to generate deck
+//function to shuffle cards
+//function count points in hand
+//function to compare dealer hand vs player hand
+//function to deal: array.pop to deal
+//function to clear table (on deal)
+//remember ace is 1 or 11
+//player blackjack automatically wins
